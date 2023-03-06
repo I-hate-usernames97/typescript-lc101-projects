a8a9591f8800b0a865a9bce7bdfe2c77027bc1f0
chrishahn <chrishahn97@outlookcom>
2023-03-06 09:16:25 -0600

    done with Astronaut.ts and Cargo.ts

---
+++ studio/Rocket.ts
@@ -0,0 +1,22 @@
+import { Astronaut } from './Astronaut'
+import { Cargo } from './Rocket'
+
+export class Rocket{
+name: string;
+totalCapacityKg: number;
+cargoItems: Cargo = [];
+astronauts: Astronaut = [];
+
+Constructor(name: string, totalCapacityKg: name){
+this.name = name;
+this.totalCapacityKg = totalCapacityKg;
+};
+
+function sumMass ( items: Payload[] ): number{
+
+}
+
+
+
+
+};
